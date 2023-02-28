0919919176abb9206d0e9f3ef9c7d5f24755cc0c
dsachi31 <dsachi31@gmail.com>
2023-02-28 00:47:07 -0800

    Added sample code

---
+++ themedroute/src/main/java/com/jsp/tmr/entities/UserEntity.java
@@ -0,0 +1,13 @@
+package com.jsp.tmr.entities;
+
+
+public class UserEntity {
+	
+	private String userName;
+	private String email;
+	private String password;
+	private int age;
+	private String address;
+	private long phno;
+
+}
